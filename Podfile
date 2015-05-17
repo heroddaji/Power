4c728d05a316fbc340161ff165e7f3baaf29b744
daitran <tranhoangdainguyen@gmail.com>
2015-05-17 23:31:38 +0900

    add first item KxMenu

---
+++ Podfile
@@ -0,0 +1,10 @@
+use_frameworks!
+inhibit_all_warnings!
+
+
+pod 'MaterialKit'
+pod 'CustomBadge'
+pod 'E84PopOutMenu'
+pod 'KxMenu'
+pod 'AKPickerView'
+
---
+++ Power/Power-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Power-Bridging-Header.h
+//  Power
+//
+//  Created by daitran on 5/17/15.
+//  Copyright (c) 2015 daitran. All rights reserved.
+//
+
+#ifndef Power_Power_Bridging_Header_h
+#define Power_Power_Bridging_Header_h
+
+#include "KxMenuViewController.h"
+
+#endif
